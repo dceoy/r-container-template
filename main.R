e25767370d8bb2cdfca1af1e9a8e67961a27e141
dceoy <dnarsil+github@gmail.com>
2018-09-03 20:49:30 +0900

    Merge R/ and Rmd/ into src/

--- main.R
+++ main.R
@@ -20,17 +20,32 @@ script_version <- 'v0.0.1'
 fetch_script_root <- function() {
   ca <- commandArgs(trailingOnly = FALSE)
   fa <- ca[grepl('^--file=', ca)]
-  return(paste0(ifelse(length(fa) == 1, dirname(sub('--file=', '', fa)), getwd()), '/'))
+  if (length(fa) == 1) {
+    f <- sub('--file=', '', fa)
+    l <- Sys.readlink(f)
+    if (is.na(l)) {
+      script_path <- normalizePath(f)
+    } else if (startsWith(l, '/')) {
+      script_path <- normalizePath(l)
+    } else {
+      script_path <- normalizePath(file.path(dirname(f), l))
+    }
+    return(dirname(script_path))
+  } else {
+    return(normalizePath(getwd()))
+  }
 }
 
 
-main <- function(opts, rscripts = NULL, rmd = NULL, pkgs = NULL, root_dir = fetch_script_root()) {
+main <- function(opts, rscripts = NULL, rmd = NULL, pkgs = NULL,
+                 root_dir = fetch_script_root()) {
   options(warn = 1)
 
-  if(opts[['--debug']]) print(opts)
-  n_thread <- ifelse(is.null(opts[['--thread']]), parallel::detectCores(), opts[['--thread']])
+  if (opts[['--debug']]) print(opts)
+  n_thread <- ifelse(is.null(opts[['--thread']]),
+                     parallel::detectCores(), opts[['--thread']])
 
-  if(! is.null(opts[['--seed']])) {
+  if (! is.null(opts[['--seed']])) {
     message('>>> Set a random seed')
     set.seed(opts[['--seed']])
     message(opts[['--seed']])
@@ -39,27 +54,24 @@ main <- function(opts, rscripts = NULL, rmd = NULL, pkgs = NULL, root_dir = fetc
   message('>>> Load functions')
   all_pkgs <- union(c('devtools', 'rmarkdown', 'stringr', 'tidyverse'), pkgs)
   print(suppressMessages(sapply(all_pkgs, require, character.only = TRUE)))
-  lapply(str_c(root_dir, rscripts), source)
+  lapply(file.path(root_dir, rscripts), source)
 
   message('>>> Set io directories')
   dirs <- lapply(c(i = '--in', o = '--out'),
-                 function(f, opts) {
-                   return(ifelse(str_detect(opts[[f]], '/$'), opts[[f]], str_c(opts[[f]], '/')))
-                 },
-                 opts = opts)
+                 function(f, opts) return(opts[[f]]), opts = opts)
   print(unlist(dirs), quote = FALSE)
 
   message('>>> Make output directories')
-  print(sapply(str_c(dirs$o,
-                     c('bib/', 'csv/', 'docx/', 'html/', 'md/', 'pdf/', 'png/', 'rds/', 'svg/',
-                       'txt/')),
+  print(sapply(file.path(dirs$o,
+                         c('bib', 'csv', 'docx', 'html', 'md', 'pdf', 'png',
+                           'rds', 'svg', 'txt')),
                dir.create, showWarnings = opts[['--debug']], recursive = TRUE))
 
   message('>>> Write session information')
-  write_session(dir = str_c(dirs$o, 'txt/'))
-  write_pkg_bib(all_pkgs, dir = str_c(dirs$o, 'bib/'))
+  write_session(dir = file.path(dirs$o, 'txt'))
+  write_pkg_bib(all_pkgs, dir = file.path(dirs$o, 'bib'))
 
-  if(n_thread > 1) {
+  if (n_thread > 1) {
     message('>>> Make a cluster')
     registerDoParallel(cl <- makeCluster(n_thread))
     message(n_thread)
@@ -70,19 +82,20 @@ main <- function(opts, rscripts = NULL, rmd = NULL, pkgs = NULL, root_dir = fetc
   # start_some_analyses()
   #
 
-  if(! is.null(rmd)) {
+  if (! is.null(rmd)) {
     message('>>> Render Rmarkdown')
-    render_rmd(str_c(root_dir, rmd), out_dir = dirs$o, quiet = ! opts[['--debug']])
+    render_rmd(file.path(root_dir, rmd),
+               out_dir = dirs$o, quiet = ! opts[['--debug']])
   }
 
-  if(n_thread > 1) {
+  if (n_thread > 1) {
     message('>>> Stop a cluster')
     stopCluster(cl)
   }
 }
 
 
-if(interactive()) {
+if (interactive()) {
   #
   # message('>>> Start interactive anayses')
   # prepare_interactive_analyses()
@@ -90,6 +103,6 @@ if(interactive()) {
 } else {
   require('docopt', quietly = TRUE)
   main(opts = docopt::docopt(doc, version = script_version),
-       rscripts = c('R/util.R'), rmd = 'Rmd/index.Rmd',
+       rscripts = c('src/util.R'), rmd = 'src/index.Rmd',
        pkgs = c('doParallel', 'foreach', 'gridExtra'))
 }
--- src/util.R
+++ src/util.R
@@ -8,48 +8,48 @@ name_v <- function(v, name) {
   return(v)
 }
 
-to_svg <- function(graph, file, w = 12, h = 9, svg2png = TRUE, svg2pdf = TRUE) {
-  svg(file, width = w, height = h)
+to_svg <- function(graph, path, w = 12, h = 9, svg2png = TRUE, svg2pdf = TRUE) {
+  svg(path, width = w, height = h)
   graph
   dev.off()
-  message(str_c('  ', file))
-  if(svg2png) {
-    png_file <- str_replace_all(file, 'svg', 'png')
-    system(str_c('rsvg-convert -f png -o ', png_file, ' -w 1000 ', file))
-    message(str_c('  ', png_file))
+  message(str_c('  ', path))
+  if (svg2png) {
+    png_path <- str_replace_all(path, 'svg', 'png')
+    system(str_c('rsvg-convert -f png -o ', png_path, ' -w 1000 ', path))
+    message(str_c('  ', png_path))
   }
-  if(svg2pdf) {
-    pdf_file <- str_replace_all(file, 'svg', 'pdf')
-    system(str_c('rsvg-convert -f pdf -o ', pdf_file, ' ', file))
-    message(str_c('  ', pdf_file))
+  if (svg2pdf) {
+    pdf_path <- str_replace_all(path, 'svg', 'pdf')
+    system(str_c('rsvg-convert -f pdf -o ', pdf_path, ' ', path))
+    message(str_c('  ', pdf_path))
   }
 }
 
-to_rds <- function(object, file, use_cache = FALSE) {
-  if(use_cache && file.exists(file)) {
-    return(readRDS(file))
+to_rds <- function(object, path, use_cache = FALSE) {
+  if (use_cache && file.exists(path)) {
+    return(readRDS(path))
   } else {
-    saveRDS(object, file = file)
-    message(str_c('  ', file))
+    saveRDS(object, file = path)
+    message(str_c('  ', path))
     return(object)
   }
 }
 
-to_csv <- function(df, file, append = FALSE, use_cache = FALSE) {
-  if(use_cache && file.exists(file)) {
-    return(readr::read_csv(file))
+to_csv <- function(df, path, append = FALSE, use_cache = FALSE) {
+  if (use_cache && file.exists(path)) {
+    return(readr::read_csv(path))
   } else {
-    readr::write_csv(df, path = file, append = append)
-    message(str_c('  ', file))
+    readr::write_csv(df, path = path, append = append)
+    message(str_c('  ', path))
     return(df)
   }
 }
 
 write_session <- function(dir = './') {
-  r_version_txt <- str_c(dir, 'r_version.txt')
+  r_version_txt <- file.path(dir, 'r_version.txt')
   suppressMessages(capture.output(version, file = r_version_txt))
   message(str_c('  ', r_version_txt))
-  session_info_txt <- str_c(dir, 'session_info.txt')
+  session_info_txt <- file.path(dir, 'session_info.txt')
   suppressMessages(capture.output(devtools::session_info(),
                                   file = session_info_txt))
   message(str_c('  ', session_info_txt))
@@ -58,7 +58,7 @@ write_session <- function(dir = './') {
 write_pkg_bib <- function(pkgs, dir = './') {
   sapply(union('base', pkgs),
          function(p) {
-           path <- str_c(dir, p, '.bib')
+           path <- file.path(dir, str_c(p, '.bib'))
            write(toBibtex(citation(p)), file = path)
            message(str_c('  ', path))
          })
@@ -70,9 +70,10 @@ render_rmd <- function(path, out_dir, quiet = FALSE) {
               list(ext = 'pdf', format = 'pdf_document'),
               list(ext = 'md', format = 'md_document')),
          function(l, path, name) {
-           render(path, output_dir = str_c(out_dir, l$ext),
+           render(path, output_dir = file.path(out_dir, l$ext),
                   output_format = l$format, quiet = quiet)
-           message(str_c('  ', out_dir, l$ext, '/', name, '.', l$ext))
+           message(str_c('  ',
+                         file.path(out_dir, l$ext, str_c(name, '.', l$ext))))
          },
          path = path,
          name = str_replace(basename(path), '.Rmd', ''))
